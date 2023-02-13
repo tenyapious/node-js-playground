a45f4ff7335a1b70527517deb495433450b632d3
tenyapious <tenyapious@gmail.com>
2023-02-13 18:57:26 -0500

    first commit

---
+++ global-modules/os.js
@@ -0,0 +1,16 @@
+const os = require("os");
+
+// info about current user
+const user = os.userInfo();
+// console.log(user);
+
+// console.log(os.uptime());
+
+const currentOS = {
+	name: os.type(),
+	release: os.release(),
+	totalMem: os.totalmem(),
+	freeMem: os.freemem(),
+};
+
+// console.log(currentOS);
